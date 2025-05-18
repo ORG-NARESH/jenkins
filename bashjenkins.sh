6472f59d79a1a8b92f45667c20027c8220e452b8
naresh <Naresh@gmail.com>
2025-05-18 22:46:47 +0530

    jenkins file updated

--- bashjenkins.sh
+++ bashjenkins.sh
@@ -2,6 +2,8 @@
 
 set -e  # Exit on any command failure
 
+sudo set-hostname jenkins
+
 echo "=== Checking Java version (pre-install) ==="
 java -version || echo "Java not found, proceeding with installation..."
 
@@ -50,7 +52,30 @@ dnf install git -y
 
 
 
-sudo growpart /dev/xvda 4
-sudo lvextend -l +60%FREE /dev/mapper/RootVG-homeVol
-sudo lvextend -l +1000%FREE /dev/mapper/RootVG-varVol
+# sudo growpart /dev/xvda 4
+# sudo lvextend -l +60%FREE /dev/mapper/RootVG-homeVol
+# sudo lvextend -l +1000%FREE /dev/mapper/RootVG-varVol
+# sudo xfs_growfs  /var ; sudo xfs_growfs  /home
+
+#Add the disk ( On UI ) 
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +20%FREE /dev/mapper/RootVG-homeVol
+sudo lvextend -l +60%FREE /dev/mapper/RootVG-varTmpVol
+#sudo lvextend -l +30%FREE /dev/mapper/RootVG-varVol
 sudo xfs_growfs  /var ; sudo xfs_growfs  /home
+df -h
+
+After attaching volume to ec2 - one disk is created
+To convert disk to parttition
+sudo gdisk /dev/nvme1n1
+sudo partprobe
+sudo pvcreate /dev/nvme1n1p1
+sudo vgextend RootVG /dev/nvme1n1p1
+sudo pvs
+sudo vgs -o +devices
+lsblk
+sudo lvextend -l +40%FREE /dev/mapper/RootVG-varVol
+lsblk
+sudo lvextend -l +40%FREE /dev/mapper/RootVG-varTmpVol
+lsblk
+sudo lvextend -l +40%FREE /dev/mapper/RootVG-logVol
